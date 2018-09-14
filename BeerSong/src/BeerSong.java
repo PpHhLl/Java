6e28ee89b6a6f01ec681ad2235caa6d525860355
PpHhLl <43137564+PpHhLl@users.noreply.github.com>
2018-09-14 14:33:57 +0800

    Java第一次课后作业

---
+++ BeerSong/src/BeerSong.java
@@ -0,0 +1,34 @@
+
+ public class BeerSong {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		int n=99;
+		String s="bottles";
+for(int i=n;n>=0;n--)
+{
+	if(n==1)
+	{
+	s="bottle";
+	}
+	if(n!=0)
+	{
+			System.out.println(n+" "+s+" "+"of beer on the wall,"+n+" "+s+" "+"of beer.");
+			System.out.println("Take one down.");
+			System.out.println("Pass it around.");
+	}
+	if(n==2)
+	{
+		s="bottle";
+	}
+	if(n!=1&&n!=0)
+	{
+	System.out.println(n-1+" "+s+" "+"of beer on the wall.\n");
+	}
+	if(n==1)
+		System.out.println("No more bottles of beer on the wall.\n");
+}
+	}
+
+}
+
